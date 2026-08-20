34456403945a63c770635c75fffb39e1738553ac
mstrangtrieueducation-droid <mstrangtrieueducation-droid@users.noreply.github.com>
2026-08-20 19:49:20 +0700

    Publish expanded IELTS Reading site

---
+++ config.js
@@ -0,0 +1,8 @@
+/*
+ * Cấu hình công khai của giao diện GitHub Pages.
+ * URL /exec bên dưới là bản triển khai chính thức của Google Apps Script.
+ */
+window.IELTS_READING_CONFIG = Object.freeze({
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbz4oph00IB6X9SlUYrK65Vsjrnd_mfm2wUg-BKZCLQnBpT6kKA-sTmFIHK1HY3ZURI/exec",
+  vocabFormUrl: "https://docs.google.com/forms/d/e/1FAIpQLSenCUUUZYcpZowQEpXCkJliGB15dmbiiWef2w8XoFnIABdq8Q/viewform?usp=pp_url",
+});
